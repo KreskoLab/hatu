a9f12aa927e8100b3f5be14bbf54ec0bcc178cd1
KreskoLab <redeezol@gmail.com>
2021-02-11 22:51:53 +0200

    Add files via upload

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -1,28 +1,39 @@
 <template>
-<b-pagination :total="count" :per-page="24" range-before="3" range-after="3" order="is-centered">
+<b-pagination v-model="page" :total="count" :per-page="24" range-before="2" range-after="2" order="is-centered">
 
     <b-pagination-button
         slot-scope="props"
         :page="props.page"
         :id="`page${props.page.number}`"
         tag="nuxt-link"
-        :to="`/explore/${props.page.number}`">
+        :to="`/?page=${props.page.number}`">
         {{ (props.page.number) }}
     </b-pagination-button>
 
-    <b-pagination-button slot="previous" slot-scope="props" :page="props.page" tag="nuxt-link" :to="`/explore/${props.page.number}`">
+    <b-pagination-button slot="previous" slot-scope="props" :page="props.page" tag="nuxt-link" :to="`/?page=${props.page.number}`">
       <b-icon icon="chevron-left"></b-icon>
     </b-pagination-button>
 
-    <b-pagination-button slot="next" slot-scope="props" :page="props.page" tag="nuxt-link" :to="`/explore/${props.page.number}`">
+    <b-pagination-button slot="next" slot-scope="props" :page="props.page" tag="nuxt-link" :to="`/?page=${props.page.number}`">
       <b-icon icon="chevron-right"></b-icon>
     </b-pagination-button>            
+    
 </b-pagination>   
 </template>
 
 <script>
 export default {
-  props: ['count', 'page']
+  props: ['count'],
+  computed:{
+    page(){
+      if (!this.$route.query.page){
+        return this.$route.query.page = 1
+      }
+      else{
+        return Number(this.$route.query.page)
+      }
+    }
+  }
 }
 
 </script>
