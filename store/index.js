8e8632f5c7979e6c6a693361455d3ab2d10d4307
KreskoLab <redeezol@gmail.com>
2021-02-13 22:59:55 +0200

    sort

--- store/index.js
+++ store/index.js
@@ -2,13 +2,17 @@
     city: {
       name: "Київ",
       code: "kvartiry-kiev"
-    }
+    },
+    sort: 2
   })
   
   export const mutations = {
     updateCity(state, newCity){
       this.$cookies.set('city', newCity)
       state.city = newCity
+    },
+    updateSort(state, newSort){
+      state.sort = newSort
     }
   }
 
@@ -26,5 +30,8 @@
   export const getters = {
     getCity: (state) => {
       return state.city
+    },
+    getSort: (state) => {
+      return state.sort
     }
   }
